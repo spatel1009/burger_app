b24c229a5162b17449e035d8868ef58e14c5c746
spatel1009 <spatel1009@gmail.com>
2019-05-30 15:02:57 -0400

    updated main font, created layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+
+const layout = ( props ) => (
+  <Fragment>
+    <div>
+      Toolbar, SideDrawer, Backdrop
+    </div>
+
+    <main>
+      {props.children}
+    </main>
+  </Fragment>
+);
+
+export default layout;
