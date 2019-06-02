f008f5eee404beb1189de88cf6515d0660ff3375
spatel1009 <spatel1009@gmail.com>
2019-06-02 18:19:42 -0400

    added backdrop, ability to exit modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import classes from './Modal.module.css';
+import Backdrop from '../Backdrop/Backdrop';
+
 
 //a modal window is a graphical control element subordinate to an application's main window
 
 const modal = (props) => (
-  <div 
-    className={classes.Modal}
-    style={{
-      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: props.show ? '1' : '0'
-    }}
-  >
-    {props.children}
-  </div>
+  <Fragment>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div 
+      className={classes.Modal}
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0'
+      }}
+    >
+      {props.children}
+    </div>
+  </Fragment>
 );
 
 export default modal;
