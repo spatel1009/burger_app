b7fb964195eedc81a1c12b3401c84ea88c1f9bc9
spatel1009 <spatel1009@gmail.com>
2019-06-02 18:40:27 -0400

    add button component

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Button.module.css';
+
+const button = (props) => (
+  <button
+    // array due to conditionally render success or danger button
+    className={[classes.Button, classes[props.btnType]].join(' ')}
+    onClick={props.clicked}
+  >
+    {props.children}
+  </button>
+);
+
+export default button;
