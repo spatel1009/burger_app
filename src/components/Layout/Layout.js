aaf17f4cad0612f0addc879733158529865815d5
spatel1009 <spatel1009@gmail.com>
2019-06-03 15:57:01 -0400

    added ability to hide sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,28 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-  <Fragment>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Fragment>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
 
-export default layout;
+  render() {
+    return (
+      <Fragment>
+        <Toolbar />
+        <SideDrawer 
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Fragment>
+  )};
+};
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import BackDrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
-
+  // added with a join below because can not add classes as an array of strings, but only as a single string
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
 
 
   return (
-    <div className={classes.SideDrawer}>
-    <div className={classes.Logo}>
-      <Logo />
-    </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Fragment>
+      <BackDrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Fragment>
   );
 };
 
