b1b4f50a4933b20d2a1aba2c92af462585ea5e21
spatel1009 <spatel1009@gmail.com>
2019-06-04 15:59:51 -0400

    added spinner in cases of loading

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 
 // Global constants should be all caps
@@ -24,7 +25,8 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 3,
     purchaseable: false,
-    purchasing: false
+    purchasing: false,
+    loading: false
   }
 
   updatePurchaseState (ingredients) {
@@ -83,6 +85,8 @@ class BurgerBuilder extends Component {
   };
 
   purchaseConfirmHandler = () => {
+    this.setState({loading: true});
+
     const order = {
       ingredients: this.state.ingredients,
       price: this.state.totalPrice.toFixed(2),
@@ -99,8 +103,12 @@ class BurgerBuilder extends Component {
     }
 
     axios.post('/orders.json', order)
-      .then(response => console.log(response))
-      .catch(error => console.log(error));
+      .then(response => {
+        this.setState({loading: false, purchasing: false});
+      })
+      .catch(error => {
+        this.setState({loading: false, purchasing: false});
+      });
   };
 
   render () {
@@ -111,17 +119,23 @@ class BurgerBuilder extends Component {
       disabledInfo[key] = disabledInfo[key] <= 0
     }
 
+    let orderSummary = <OrderSummary 
+      ingredients={this.state.ingredients}
+      price={this.state.totalPrice}
+      purchaseCanceled={this.purchaseCancelHandler}
+      purchaseConfirm={this.purchaseConfirmHandler} />
+
+    if (this.state.loading) {
+      orderSummary = <Spinner />
+    }
+
     return (
       <Fragment>
         <Modal 
           show={this.state.purchasing} 
           modalClosed={this.purchaseCancelHandler}
         >
-          <OrderSummary 
-            ingredients={this.state.ingredients}
-            price={this.state.totalPrice}
-            purchaseCanceled={this.purchaseCancelHandler}
-            purchaseConfirm={this.purchaseConfirmHandler} />
+          {orderSummary}
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls 
