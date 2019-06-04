6aee086186dac615e7326eb157707a1231fb3749
spatel1009 <spatel1009@gmail.com>
2019-06-04 16:20:19 -0400

    created global error handling with hoc used as wrapping

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 
 // Global constants should be all caps
@@ -151,4 +152,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,42 @@
+import React, {Fragment, Component} from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null
+    }
+
+    componentDidMount () {
+      // Add request to clear errors anytime a request is made
+      axios.interceptors.request.use(req => {
+        this.setState( { error: null } );
+        return req;
+      })
+      // Below the shortest possible way to return the response, which is necessary.
+      axios.interceptors.response.use(resp => resp, error => {
+        this.setState( { error: error } );
+      });
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState( { error: null } );
+    }
+
+    render () {
+      return (
+        <Fragment>
+          <Modal 
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Fragment>
+      );
+    }
+  }
+}
+
+export default withErrorHandler;
